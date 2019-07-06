955f7ea8c494a57c6c0ff74d358a3230df205a8c
Katherine Wang <kwang004@gmail.com>
2019-07-05 22:13:05 -0700

    Updated port number from 3000 -> 3001

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 const bodyParser = require('body-parser');
 const sequelize = require('sequelize');
 const db = require('./db/index.js');
